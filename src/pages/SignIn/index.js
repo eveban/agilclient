963d7c120693905e1881b8c23eaa4c414c20fd62
eveban <everson_gsilva@yahoo.com.br>
2019-11-01 20:10:19 -0300

    Criando o Login personalizado

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import {
+  Button,
+  CardContent,
+  Checkbox,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import logo from '~/assets/logo.svg';
+import { Content, Card } from './styles';
 
 export default function SignIn() {
-  return <Container></Container>;
+  function handleSubmit() {}
+
+  return (
+    <Content>
+      <Card>
+        <img src={logo} alt="GoBarber" />
+        <Typography>LOGIN TO YOUR ACCOUNT</Typography>
+        <Form name="loginForm" onSubmit={handleSubmit}>
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Sua senha secreta"
+          />
+          <Button>Olá</Button>
+        </Form>
+      </Card>
+    </Content>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -4,4 +4,7 @@ export const Wrapper = styled.div`
   height: 100%;
   /*background: linear-gradient(-90deg, #7159c1, #ab59c1);*/
   background: linear-gradient(0deg, #263238, #cfd8dc);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;

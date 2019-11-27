cbe738f1e33bf6a6879a8be0095761c08a7efe2d
eveban <everson_gsilva@yahoo.com.br>
2019-11-27 20:43:46 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import './config/ReactotronConfig';
+import '~/config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
 import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <PersistGate persistor={persistor}>
         <Router history={history}>
           <Routes />
+          <GlobalStyle />
         </Router>
       </PersistGate>
     </Provider>

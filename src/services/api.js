3efe4025829be720441005693177c4123cbb3df0
eveban <everson_gsilva@yahoo.com.br>
2020-01-21 10:11:27 -0300

    Retornando CORS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://187.9.38.146:3333',
+  baseURL: 'https://cors-anywhere.herokuapp.com/' + 'http://187.9.38.146:3333',
   // 'https://cors-anywhere.herokuapp.com/'+
 });
 

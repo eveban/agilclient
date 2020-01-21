2df22003493aadadd1ca2d6621afd00555dd81a0
Cesar Alexandre <xandepir@gmail.com>
2020-01-20 22:05:13 -0300

    ajustes no service api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://187.9.38.146:3333',
+  baseURL: 'https://cors-anywhere.herokuapp.com/'+'http://www.angelelli.com.br:3333',
 });
 
 export default api;

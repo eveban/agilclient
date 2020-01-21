a918c380bf79d2875738e3a789b0455c13663a29
eveban <everson_gsilva@yahoo.com.br>
2020-01-21 10:08:18 -0300

    Alterando angelelli.com.br para IP

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://www.angelelli.com.br:3333',
+  baseURL: 'http://187.9.38.146:3333',
   // 'https://cors-anywhere.herokuapp.com/'+
 });
 

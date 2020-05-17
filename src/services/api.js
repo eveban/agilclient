b90eba9d4845692054bf098dce8cf45edcf8c750
eveban <everson_gsilva@yahoo.com.br>
2020-05-17 08:38:12 -0300

    Alteração do caminho da API- Remoção do cors do Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://187.9.38.146:3333',
+  // baseURL: 'https://cors-anywhere.herokuapp.com/http://187.9.38.146:3333',
+  baseURL: 'https:rotas.angelelli.com.br',
   // 'https://cors-anywhere.herokuapp.com/'+
 });
 

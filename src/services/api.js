0e1d60b2e175f974f2a2048559fa515b2a5da7e8
eveban <everson_gsilva@yahoo.com.br>
2020-05-17 08:43:50 -0300

    Correção da API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://cors-anywhere.herokuapp.com/http://187.9.38.146:3333',
-  baseURL: 'https:rotas.angelelli.com.br',
+  baseURL: 'https://rotas.angelelli.com.br',
   // 'https://cors-anywhere.herokuapp.com/'+
 });
 
